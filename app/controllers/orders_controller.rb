e9a1bb925e34f25212d556cea88fb9082bcd065f
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-01-23 19:12:54 +0900

    ログアウト状態の購入ページへの制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_order, only: [:index, :create]
   def index
-    @item = Item.find(params[:item_id])
     @order_form = OrderForm.new
     
   end
 
   def create
     @order_form = OrderForm.new(order_params)
-    @item = Item.find(params[:item_id])
     if @order_form.valid?
       pay_item
       @order_form.save
@@ -20,6 +20,10 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(token: params[:token],user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_order
+    @item = Item.find(params[:item_id])
   end
 
   def pay_item

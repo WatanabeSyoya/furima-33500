b2635b9e99f969e851ed3ae810ee7fd2e6f378cc
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-01-23 12:43:14 +0900

    ordersコントローラー作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,35 @@
+class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new
+    
+  end
+
+  def create
+    @order_form = OrderForm.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(token: params[:token],user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_16ab505af1cf6469bc1bb106"  
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],   
+      currency: 'jpy'              
+    )
+  end
+
+
+end
